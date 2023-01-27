e3cde50a49d8d727812c667155f49768d6655a6a
Richard Pascual <ricpascual@gmail.com>
2023-01-27 08:53:09 -0500

    proyecto concluido con lazy load

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy"
 
-console.log('Happy hacking :)')
+const maximun = 122
+const minimun = 1
+
+const random = () => Math.floor(Math.random() * (maximun * minimun )) + minimun
+const createImageNdode = () => {
+    
+
+    const imagen = document.createElement('img')
+    imagen.className = "mx-auto"
+    imagen.width = "320"
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg` // todo
+
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "bg-gray-300";
+    imageWrapper.style.minHeight = "100px";
+    imageWrapper.style.display = "inline-block";
+
+    imageWrapper.appendChild(imagen);
+
+    appendedImages++;
+    printLog();
+
+    return imageWrapper
+}
+
+
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("#add");
+const cleanButton = document.querySelector("#remove");
+
+const addImage = () => {
+    const newImage = createImageNdode()
+    mountNode.append(newImage)
+    registerImage(newImage)
+}
+
+
+const cleanImages = () => {
+
+    console.log(mountNode.childNodes);
+
+    [...mountNode.childNodes].forEach(child => {
+        child.remove();
+    }) 
+
+}
+
+addButton.addEventListener("click", addImage)
+cleanButton.addEventListener("click", cleanImages)
